6f2a45c95e99de55ed66cf0d114b73d3cd5de2fa
Akshat Pande <akshat.pande@outlook.com>
2023-09-27 20:26:56 +0100

    Added Page Title

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import streamlit as st 
 
-
+st.set_page_config(page_title="Currency Change", page_icon=":moneybag:", layout="centered", initial_sidebar_state="expanded")
 
 def dpMakeChange(coinValueList,change,mincoins,coinsUsed):
     for counts in range(change+1):
